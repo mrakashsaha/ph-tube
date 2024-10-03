f7ec574fb07cfb3927fa23f3fdfd2e9c9abc8741
mrakashsaha <mr.akashsaha@gmail.com>
2024-10-03 14:50:41 +0600

    Button and card fetch from API

---
+++ script/video.js
@@ -0,0 +1,78 @@
+function displayCatagory (data) {
+    const categoryContainer = document.getElementById ('categories');
+    data.forEach( (item) => {
+        
+        const button = document.createElement('button');
+        button.classList ='btn';
+        button.innerText = item.category;
+        categoryContainer.appendChild (button);
+    });
+}
+
+async function loadCatagory () {
+    const res = await fetch ('https://openapi.programming-hero.com/api/phero-tube/categories');
+    const data = await res.json();
+    displayCatagory(data.categories);
+    
+
+    
+}
+
+async function loadVideos () {
+    const res = await fetch ('https://openapi.programming-hero.com/api/phero-tube/videos');
+    const data = await res.json();
+    displayVideos (data.videos);
+    
+
+    
+}
+
+
+function displayVideos (videos) {
+    const videoContainer = document.getElementById ('videos');
+    videos.forEach ((video)=> {
+        console.log (video);
+        const card = document.createElement('div');
+        card.classList = 'card card-compact shadow-xl'
+        card.innerHTML = 
+        `   
+            <figure>
+                    <img
+                    src=${video.thumbnail}
+                    alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+
+        `
+
+        videoContainer.appendChild(card);
+    })
+}
+
+loadCatagory ();
+loadVideos ();
+
+const obj = {
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
